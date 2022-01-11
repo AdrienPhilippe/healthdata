e0e6e6e22b338790c375a28a3432386ebd9c329d
Thomas Menchi <thomas.menchi@edu.esiee.fr>
2022-01-11 14:47:41 +0100

    script to init db tables

---
+++ ressources/db_init.sql
@@ -0,0 +1,36 @@
+CREATE TABLE `Patients` (
+  `id_patient` INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  `name` text NOT NULL DEFAULT 'None',
+  `firstname` text NOT NULL DEFAULT 'None',
+  `username` text NOT NULL UNIQUE DEFAULT 'None',
+  `email` text NOT NULL DEFAULT 'None',
+  `password` text NOT NULL DEFAULT 'None',
+  `age` INT NOT NULL DEFAULT 0,
+  `height` INT NOT NULL DEFAULT 0
+);
+
+CREATE TABLE `Doctors` (
+  `id_doctor` INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  `name` text NOT NULL DEFAULT 'None',
+  `firstname` text NOT NULL DEFAULT 'None',
+  `email` text NOT NULL DEFAULT 'None',
+  `password` text NOT NULL DEFAULT 'None'
+);
+
+CREATE TABLE `Relations` (
+  `id_doctor` INT NOT NULL,
+  `id_patient` INT NOT NULL,
+  FOREIGN KEY (id_doctor) REFERENCES Doctors(id_doctor),
+  FOREIGN KEY (id_patient) REFERENCES Patients(id_patient)
+);
+
+CREATE TABLE `Datas` (
+  `id_patient` INT NOT NULL DEFAULT 0,
+  `timestamp` text NOT NULL DEFAULT 0,
+  `weight` INT NOT NULL DEFAULT 0,
+  `chest` INT NOT NULL DEFAULT 0,
+  `abdomen` INT NOT NULL DEFAULT 0,
+  `hip` INT NOT NULL DEFAULT 0,
+  `heartbeat` INT NOT NULL DEFAULT 0,
+  FOREIGN KEY (id_patient) REFERENCES Patients(id_patient)
+);
